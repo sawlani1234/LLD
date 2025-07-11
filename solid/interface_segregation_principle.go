fd656405e451b4d6ccbf6911ddee81442d57b55b
SHUBHAM SAWLANI <shubhamsawlani@SHUBHAMs-MacBook-Air.local>
2025-07-12 00:57:34 +0530

    Open Closed Principle

--- solid/interface_segregation_principle.go
+++ solid/interface_segregation_principle.go
@@ -1,1 +1,20 @@
 package solid
+
+
+
+type FileWorker interface {
+     Reader()
+	 Writer()
+}
+
+// some filestypes (lets say confidential earning report) can only be read  hence 
+// no need multiple interface can be segreagted as 
+
+// Segregating interface so only required interfaces are implenented
+type Reader interface {
+	Read()
+}
+
+type Writer interface {
+	Write()
+}
--- solid/open_closed_principle.go
+++ solid/open_closed_principle.go
@@ -1,1 +1,51 @@
 package solid
+
+// import "reflect"
+
+
+type Shape interface{} 
+
+
+type Square struct {
+	length int 
+  
+}
+
+type Rectangle struct {
+	length int 
+	breadth int 
+}
+
+
+// Violates Open Closed Principle for getting area
+// // Calculates area of given shape 
+// type Area struct {
+
+// }
+
+
+// func (a Area) getArea(s Shape) int {
+
+// 	  if reflect.TypeOf(s) == reflect.TypeOf(Square{}) {
+// 		  return s.(Square).length * s.(Square).length
+
+// 	  } else if reflect.TypeOf(s) == reflect.TypeOf(Rectangle{}) {
+// 		return s.(Rectangle).length * s.(Rectangle).breadth
+// 	  }
+   
+
+// 	  return -1 
+// }
+
+// Now each new shape will just implement new funciton to get its area
+type Area interface {
+	GetArea() int 
+}
+
+func (s Square) GetArea() int {
+	return s.length*s.length
+}
+
+func (r Rectangle) GetArea() int {
+	return r.length * r.breadth
+}
