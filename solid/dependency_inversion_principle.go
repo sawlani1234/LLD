b3e4a6b8dbbc57328f7fb04cd5fe105b01930fdc
SHUBHAM SAWLANI <shubhamsawlani@SHUBHAMs-MacBook-Air.local>
2025-07-12 17:20:38 +0530

    DI

--- solid/dependency_inversion_principle.go
+++ solid/dependency_inversion_principle.go
@@ -1,1 +1,38 @@
 package solid
+
+// It says that higher level dependencies should depend on interface of the lower level depdenco
+// i.e shoukd not depend on the actual implementation 
+
+type EmailService struct {
+
+}
+
+func (e EmailService) Send() error {
+	return nil
+}
+
+
+// type NotificationService struct {
+//    emailService EmailService
+// }
+
+// now in this case lets say in future we want to have push notification to device then we might to change 
+// the class variable which will make it tougher to add code if else woould be needed on extra variabke 
+// we can do something like this take this  out to interface 
+
+type NotificationService struct {
+	senderService SenderService
+}
+
+type SenderService interface {
+	Send() error 
+}
+
+
+type PushInboxService struct {
+
+}
+
+func (p PushInboxService) Send() error {
+	return nil
+}
